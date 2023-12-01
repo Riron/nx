2797235cd535d3cef813a8e99bd461c37ca37f96
Max Kless <maxk@nrwl.io>
2023-12-01 11:12:32 +0100

    cleanup(core): add function that builds graph & reads source map in one go

--- packages/nx/src/project-graph/project-graph.ts
+++ packages/nx/src/project-graph/project-graph.ts
@@ -18,7 +18,7 @@ import {
 } from './utils/retrieve-workspace-files';
 import { readNxJson } from '../config/nx-json';
 import { unregisterPluginTSTranspiler } from '../utils/nx-plugin';
-import { writeSourceMaps } from '../utils/source-maps';
+import { writeSourceMapsToDisk } from '../utils/source-maps';
 
 /**
  * Synchronously reads the latest cached copy of the workspace's ProjectGraph.
@@ -100,7 +100,7 @@ export async function buildProjectGraphWithoutDaemon() {
     )
   ).projectGraph;
 
-  writeSourceMaps(sourceMaps);
+  writeSourceMapsToDisk(sourceMaps);
 
   unregisterPluginTSTranspiler();
 
